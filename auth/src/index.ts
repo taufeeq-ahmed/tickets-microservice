5e2a2983c6fbcc88c733a7242b5a81e5aedb2ff6
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-09 22:06:42 +0530

    route added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,9 @@ import express from "express"
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send("root")
+})
 app.listen(3000, () => {
     console.log("🟢🟢 auth-server up on port : 3000");
 })
