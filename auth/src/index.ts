b986ae7c58cb034b6a8c85c921d5290c2ca9bc3b
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-10 11:47:27 +0530

    commiting

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,9 +2,10 @@ import express from "express"
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("root")
+app.get("/api/users/current-user", (req, res) => {
+    res.send("hello there")
 })
+
 app.listen(3000, () => {
     console.log("🟢🟢 auth-server up on port : 3000");
 })
