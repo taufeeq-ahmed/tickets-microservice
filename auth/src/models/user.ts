293ab82188595c8dfbbf413db22e38a096091c45
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-11 17:49:49 +0530

    user model created

---
+++ auth/src/models/user.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+interface UserProps {
+    email: string,
+    password: string
+}
+
+interface UserDoc extends mongoose.Document {
+    email: string,
+    password: string
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(props: UserProps): UserDoc
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
+
+export default User
