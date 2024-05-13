4650e9a64047807cec8225e2e85482f7775f303c
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-13 12:13:57 +0530

    current-user route done

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import currentUserExtractor from "../middlewares/current-user-extractor";
 
 const router = express.Router()
 
-router.get("/api/users/current-user", (req, res) => {
-    res.send("current-user")
+router.get("/api/users/current-user", currentUserExtractor, (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null })
 })
 
 export { router as currentUserRouter }
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from "express";
 const router = express.Router()
 
 router.post("/api/users/signout", (req, res) => {
-    res.send("sign-out")
+    req.session = null
+    res.send("signed-out")
 })
 
 export { router as signoutRouter }
