3064da581afe6d8f4021f8c59ca44fcc79deb15b
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-14 14:35:13 +0530

    use-request hook is made

---
+++ client/hooks/use-request.ts
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import axios, { AxiosResponse, AxiosError, Method } from 'axios';
+import request from './request';
+
+interface UseRequestReturnType<T> {
+    trigger: (url: string, method: Method, body?: any) => void;
+    reset: () => void;
+    data: T | null;
+    error: AxiosError<T> | null;
+    loading: boolean;
+}
+
+const useRequest = <T>(): UseRequestReturnType<T> => {
+    const [data, setData] = useState<T | null>(null);
+    const [error, setError] = useState<AxiosError<T> | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const trigger = async (url: string, method: Method, body?: any) => {
+        setLoading(true);
+        try {
+            const response: AxiosResponse<T> = await request.request<T>({
+                url,
+                method,
+                data: body,
+            });
+            setData(response.data);
+            setError(null);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const reset = () => {
+        setData(null);
+        setError(null);
+    };
+
+    return { trigger, reset, data, error, loading };
+};
+
+export default useRequest;
