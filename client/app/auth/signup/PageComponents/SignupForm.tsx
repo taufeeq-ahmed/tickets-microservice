58bd6c1a4804b49c01ae052da405ff070a0c90ec
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-14 12:51:18 +0530

    shadcn to be adde

--- client/app/auth/signup/PageComponents/SignupForm.tsx
+++ client/app/auth/signup/PageComponents/SignupForm.tsx
@@ -1,9 +1,12 @@
+// import { Button } from '@/components/ui/button'
+// import { Input } from "@/components/ui/input"
+
 import Image from 'next/image'
 import React from 'react'
 
 function SignupForm() {
     return (
-        <div className=' w-full h-full bg-themed-grey 
+        <div className=' w-full h-full bg-themedGrey 
          flex flex-col justify-center items-center'>
             <Image
                 className='justify-center'
@@ -12,6 +15,16 @@ function SignupForm() {
                 width={100}
                 height={100}
             />
+            <form className='w-full p-20 py-2 flex flex-col gap-4'>
+                {/* <Input
+                    type='email'
+                    placeholder='Email'
+                />
+                <Input
+                    type='password'
+                    placeholder='Password'
+                /> */}
+            </form>
         </div>
     )
 }
--- client/app/auth/signup/page.tsx
+++ client/app/auth/signup/page.tsx
@@ -8,7 +8,7 @@ const SignUp = () => {
             <div className='banner w-[60%] h-full'>
                 <Banner />
             </div>
-            <div className='signup-form  w-[30%]'>
+            <div className='signup-form  w-[40%]'>
                 <SignupForm />
             </div>
         </div>
