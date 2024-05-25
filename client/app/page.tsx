476f42589c79d4465edf6332e9bcee1bf5e04fc7
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-25 19:47:51 +0530

    home page is done

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import Cookies from "universal-cookie";
 
 export default function Home() {
+  const [isLoggedin, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const cookies = new Cookies();
+    setIsLoggedIn(cookies.get('session'))
+  }, [])
+
   return (
     <main className={styles.main}>
-
+      <h1>{isLoggedin ? ("Logged In") : ("Not Logged In")}</h1>
     </main>
   );
 }
