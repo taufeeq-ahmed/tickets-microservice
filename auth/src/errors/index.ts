95d21f38b127782ace9c97e936588090f2d7d142
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-25 11:00:52 +0530

    moved

--- auth/src/errors/index.ts
+++
@@ -1,6 +0,0 @@
-import CustomError from "./custom-error";
-import DatabaseConnectionError from "./databse-connection-error";
-import NotFoundError from "./not-found-error";
-import RequestValidationError from "./request-validation-error";
-
-export { DatabaseConnectionError, RequestValidationError, CustomError, NotFoundError }
